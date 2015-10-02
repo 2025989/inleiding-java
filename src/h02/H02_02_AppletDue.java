60bd85ae19897700abb9fd4cb382e254c63af757
2025989 <anon@git.hub>
2015-10-02 17:30:48 +0200

    project restructurization

---
+++ src/h02/H02_02_AppletDue.java
@@ -0,0 +1,17 @@
+package h02;
+import java.awt.*;
+import java.applet.*;
+
+public class H02_02_AppletDue extends Applet {
+
+  public void init() {
+    setBackground(Color.white);
+  }
+
+  public void paint(Graphics g) {
+    g.setColor(Color.blue);
+    g.drawString("Daniel", 50, 60 );
+    g.setColor(Color.red);
+    g.drawString("Wezeman", 50, 70 );
+  }
+}
---
+++ src/h04/H04_05_FillArcEllips.java
@@ -0,0 +1,17 @@
+package h04;
+
+import java.awt.*;
+import java.applet.*;
+
+public class H04_05_FillArcEllips extends Applet{
+	
+	public void init(){
+		setBackground(Color.blue);
+	}
+	
+	public void paint(Graphics g){
+	g.setColor(Color.yellow);
+	g.fillArc(50, 25, 100, 150, 90, 360);
+	}
+
+}
