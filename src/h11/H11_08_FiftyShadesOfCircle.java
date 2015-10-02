ffe16b05148c872091ef4c388d5c0325f846c15f
2025989 <anon@git.hub>
2015-10-02 17:31:39 +0200

    H11

---
+++ src/h11/H11_08_FiftyShadesOfCircle.java
@@ -0,0 +1,21 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class H11_08_FiftyShadesOfCircle extends Applet {
+
+	public void init() {
+		setSize(550, 550);
+	}
+	
+	public void paint(Graphics g) {
+		int x = getWidth();
+		int y = getHeight();
+		int sz = getHeight()/110;
+		for (int i = 1; i <= 50; i++) {
+			g.drawOval(x/2-sz*i, y/2-sz*i, 1+sz*2*i, 1+sz*2*i);
+		}
+	}
+
+}
